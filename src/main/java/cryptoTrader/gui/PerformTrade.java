8dfb7968d1948a6f57d17f0f024572ae83c42fd9
moh-dar709 <102563555+moh-dar709@users.noreply.github.com>
2022-03-31 18:16:23 -0400

    refresh

--- src/main/java/cryptoTrader/gui/PerformTrade.java
+++ src/main/java/cryptoTrader/gui/PerformTrade.java
@@ -5,19 +5,13 @@ public class PerformTrade {
 	// constructor
 	public PerformTrade() {
 		
-	}
 	
 	// call api and fetch data and store in data structure 
 	
 			
 	// pass data to broker
-	
-	
-	
-	
-	
-	
-	
-	
+		
+		
+	} // end of perform trade
 	
 } // end of file
