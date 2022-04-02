7a2c1b0dd21e656521ea1f288860e7fcfcac55e3
Dexter Yan <maildexteryan@gmail.com>
2022-04-02 17:24:12 -0400

    Test push

---
+++ src/main/java/cryptoTrader/gui/LoginUI.java
@@ -0,0 +1,10 @@
+package cryptoTrader.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public interface LoginUI {
+    
+}
