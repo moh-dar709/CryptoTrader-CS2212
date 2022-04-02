376e7e5711d2e54c8b5df15aa0067976ca27f6fa
moh-dar709 <102563555+moh-dar709@users.noreply.github.com>
2022-04-02 17:39:20 -0400

    binary push

--- src/main/java/cryptoTrader/gui/MainUI.java
+++ src/main/java/cryptoTrader/gui/MainUI.java
@@ -175,15 +175,15 @@ public class MainUI extends JFrame implements ActionListener {
 
 	// displays the ui
 	public static void main(String[] args) {
-		LoginSystem login = new LoginSystem();
-		login.setPreferredSize(new Dimension(400, 400));
-		login.setBounds(10,10,370,600);
-		login.pack();
-		login.setVisible(true);
+//		LoginSystem login = new LoginSystem();
+//		login.setPreferredSize(new Dimension(400, 400));
+//		login.setBounds(10,10,370,600);
+//		login.pack();
+//		login.setVisible(true);
 		JFrame frame = MainUI.getInstance();
 		frame.setSize(900, 600);
 		frame.pack();
-		frame.setVisible(false);
+		frame.setVisible(true);
 	}
 
 	@Override
