54776b908f230234874af0329e27ddd9ce771921
dishapuri <70620362+dishapuri@users.noreply.github.com>
2022-04-01 22:23:49 -0400

    Added broker 2 strategies

--- src/main/java/cryptoTrader/gui/MainUI.java
+++ src/main/java/cryptoTrader/gui/MainUI.java
@@ -134,6 +134,7 @@ public class MainUI extends JFrame implements ActionListener {
 		remRow.setActionCommand("remTableRow");
 		remRow.addActionListener(this);
 
+
 		scrollPane.setPreferredSize(new Dimension(800, 300));
 		table.setFillsViewportHeight(true);
 		
