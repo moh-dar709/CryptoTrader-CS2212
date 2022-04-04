6e101d741df18db60a18f351625b2297587ba4cc
Dexter Yan <maildexteryan@gmail.com>
2022-04-04 17:43:13 -0400

    Javadoc comments for coin class

--- src/main/java/cryptoTrader/gui/Coin.java
+++ src/main/java/cryptoTrader/gui/Coin.java
@@ -1,35 +1,61 @@
+/**
+ * Coin defines what a coin is
+ * @author Mohammed Al-Darwish, Disha Puri, Anusha Sheikh, Dexter Yan
+ */
 package cryptoTrader.gui;
 
 public class Coin {
 
-	// instance variables
+	/**
+	 * Instance variables
+	 */
 	private String name;
 	private double price;
 	private double marketCap;
 	private double volume;
-	
-	// constructor
+
+	/**
+	 * Constructor for coin
+	 * @param name name of coin
+	 * @param p price of coin
+	 * @param mCap market cap of coin
+	 * @param vol volume trade of coin
+	 */
 	public Coin(String name, double p, double mCap, double vol) {
 		this.name = name;
 		this.price = p;
 		this.marketCap = mCap;
 		this.volume = vol;
 	} // end of constructor
-	
-	// getter name
+
+	/**
+	 * getName method
+	 * @return coin name
+	 */
 	public String getName() {
 		return this.name;
 	}
-	
-	 // getter price
+
+	/**
+	 * getPrice method
+	 * @return coin price
+	 */
 	public double getPrice() {
 		return this.price;
 	}
-	
+
+	/**
+	 * marketCap method
+	 * @return coin market cap
+	 */
 	public double getMarketCap() {
 		return this.marketCap;
 	}
-	
+
+	/**
+	 * getVolume method
+	 * @return coin volume trade
+	 */
 	public double getVolume() {
 		return this.volume;
 	}
