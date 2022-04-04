ee785dd5a7a5c831e3e93087e3565ab677c3916a
moh-dar709 <102563555+moh-dar709@users.noreply.github.com>
2022-04-04 17:37:33 -0400

    Javadoc and clean up TradingStrategies

--- src/main/java/cryptoTrader/gui/TradingStrategies.java
+++ src/main/java/cryptoTrader/gui/TradingStrategies.java
@@ -1,18 +1,31 @@
+/**
+ * TradingStrategies holds the trading algorithms for each broker and updates the trade action log accordingly
+ * @author Mohammed Al-Darwish, Disha Puri, Anusha Sheikh, Dexter Yan
+ */
+
 package cryptoTrader.gui;
 
 import java.text.DecimalFormat;
 import java.util.*;
 
 public class TradingStrategies {
-
-	// constructor
-	/*
+	
+	/**
+	 * String variable that holds today's date
+	 */
+	private String date = "1-April-2022"; // TODO change to current
+	
+	/**
+	 * This method is the TradingStrategies constructor. It directs the trade call to the appropriate broker.
 	 * 
+	 * @param broker represents the name of the broker that wants to do the trade
+	 * @param strat represents the strategy which the broker wants to do
+	 * @param coinMap represents the HashMap of coins that the broker has access to
+	 * @param database represents the action log database where all current actions are logged
 	 */
 	public TradingStrategies(String broker, String strat, HashMap<String, Coin> coinMap, ActionLog database) {
-		// commence trade
-
-		// switch to determine which strat
+		
+		// switch to determine which strategy is requested
 		switch (strat) {
 			case "Strategy-A":
 				stratA(broker, coinMap, database); // do A
@@ -33,17 +46,20 @@ public class TradingStrategies {
 		} // end of switch
 
 	} // end of constructor
-
-	// getter method to get the corresponding strat for a certain broker
-	// switch to get appropriate strat for desired broker
-
-	//Broker strats below
+	
+	/**
+	 * This method is responsible for all Strategy-A broker algorithms.
+	 * 
+	 * @param broker represents the broker name
+	 * @param coinMap represents the map of coins associated with the broker
+	 * @param database represents the action log database where the action will be logged
+	 */
 	private void stratA(String broker, HashMap<String, Coin> coinMap, ActionLog database) {
-		System.out.println("im in A");
 
-		// implement if or switch to select broker
+		// switch to select broker
 		switch(broker) {
 			//TODO Broker-1 Bitcoin
+			// if bitcoin is under 50000 and cardano is over 2, buy 10 cardano
 			case "Broker-1":
 				// get coins
 				Coin btc = coinMap.get("bitcoin");
@@ -51,14 +67,14 @@ public class TradingStrategies {
 				// do calculations
 				if(btc != null && ada != null) {
 					if((btc.getPrice() <= 50000) && (ada.getPrice() > 2)) {
-						successfulTrade(broker, "Strategy-A", "ADA", "Buy", "10", truncate(ada.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-A", "ADA", "Buy", "10", truncate(ada.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-A", "ADA","1-April-2022",database);
+						failedTrade(broker, "Strategy-A", "ADA",this.date,database);
 					}
 				}
 				else {
-					badInput(broker,"Strategy-A","1-April-2022",database);
+					badInput(broker,"Strategy-A",this.date,database);
 				}
 				break;
 
@@ -67,17 +83,17 @@ public class TradingStrategies {
 			case "Broker-2"	:
 				Coin eth = coinMap.get("ethereum");
 				Coin ltc = coinMap.get("litecoin");
-
+				
 				if ((eth != null) && (ltc != null)) {
 					if ((eth.getPrice() > 3300) && (ltc.getPrice() < 90)) {
-						successfulTrade(broker, "Strategy-A", "LTC", "Sell", "15", truncate(ltc.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-A", "LTC", "Sell", "15", truncate(ltc.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-A", "LTC","1-April-2022",database);
+						failedTrade(broker, "Strategy-A", "LTC",this.date,database);
 					}
 				}
 				else {
-					badInput(broker,"Strategy-A","1-April-2022",database);
+					badInput(broker,"Strategy-A",this.date,database);
 				}
 				break;
 
@@ -89,14 +105,14 @@ public class TradingStrategies {
 
 				if ((tether!=null) && (terra!=null)){
 					if ((tether.getPrice() > 1) && (terra.getPrice()<100)){
-						successfulTrade(broker, "Strategy-A", "Terra", "Sell", "3", truncate(terra.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-A", "Terra", "Sell", "3", truncate(terra.getPrice()), this.date, database);
 					}
 					else{
-						failedTrade(broker, "Strategy-A", "Terra", "1-April-2022", database);
+						failedTrade(broker, "Strategy-A", "Terra", this.date, database);
 					}
 				}
 				else{
-					badInput(broker, "Strategy-A", "1-April-2022", database);
+					badInput(broker, "Strategy-A", this.date, database);
 				}
 				break;
 
@@ -107,21 +123,27 @@ public class TradingStrategies {
 				Coin sol = coinMap.get("solana");
 				if((doge!= null) && (sol!= null)){
 					if((doge.getPrice()>0.1) && (sol.getPrice() < 150)){
-						successfulTrade(broker, "Strategy-A", "SOL", "Buy", "5", truncate(sol.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-A", "SOL", "Buy", "5", truncate(sol.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-A", "SOL", "1-April-2022", database);
+						failedTrade(broker, "Strategy-A", "SOL", this.date, database);
 					}
 				}
 				else {
-					badInput(broker, "Strategy-A", "1-April=2022", database);
+					badInput(broker, "Strategy-A", this.date, database);
 				}
 
 		} // end of switch
 	} // end of stratA
 
+	/**
+	 * This method is responsible for all Strategy-B broker algorithms.
+	 * 
+	 * @param broker represents the broker name
+	 * @param coinMap represents the map of coins associated with the broker
+	 * @param database represents the action log database where the action will be logged
+	 */
 	private void stratB(String broker, HashMap<String, Coin> coinMap, ActionLog database) {
-		System.out.println("im in B");
 
 		switch(broker) {
 			//TODO Broker-1 Bitcoin
@@ -132,14 +154,14 @@ public class TradingStrategies {
 
 				if ((btc != null) && (dot != null)) {
 					if ((btc.getPrice() < 59200) && (dot.getPrice() > 25)) {
-						successfulTrade(broker, "Strategy-B", "DOT", "Buy", "6", truncate(dot.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-B", "DOT", "Buy", "6", truncate(dot.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-B", "DOT","1-April-2022", database );
+						failedTrade(broker, "Strategy-B", "DOT",this.date, database );
 					}
 				}
 				else {
-					badInput(broker, "Strategy-B", "1-April-2022", database);
+					badInput(broker, "Strategy-B", this.date, database);
 				}
 				break;
 
@@ -151,14 +173,14 @@ public class TradingStrategies {
 
 				if ((eth != null) && (sol != null)) {
 					if ((eth.getPrice() < 3300) && (sol.getPrice() > 100)) {
-						successfulTrade(broker, "Strategy-B", "SOL", "Buy", "4", truncate(sol.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-B", "SOL", "Buy", "4", truncate(sol.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-B", "SOL","1-April-2022",database);
+						failedTrade(broker, "Strategy-B", "SOL",this.date,database);
 					}
 				}
 				else {
-					badInput(broker,"Strategy-B","1-April-2022",database);
+					badInput(broker,"Strategy-B",this.date,database);
 				}
 				break;
 
@@ -170,14 +192,14 @@ public class TradingStrategies {
 
 				if ((usdt != null) && (avax != null)) {
 					if ((usdt.getPrice() < 1) && (avax.getPrice() > 7000)) {
-						successfulTrade(broker, "Strategy-B", "AVAX", "Buy", "6", truncate(avax.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-B", "AVAX", "Buy", "6", truncate(avax.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-B", "AVAX","1-April-2022", database );
+						failedTrade(broker, "Strategy-B", "AVAX",this.date, database );
 					}
 				}
 				else {
-					badInput(broker, "Strategy-B", "1-April-2022", database);
+					badInput(broker, "Strategy-B", this.date, database);
 				}
 				break;
 
@@ -188,18 +210,25 @@ public class TradingStrategies {
 				btc = coinMap.get("bitcoin");
 				if((doge!= null) && (btc!= null)){
 					if((doge.getPrice()<0.2) && (btc.getPrice() > 55000)){
-						successfulTrade(broker, "Strategy-B", "BTC", "Sell", "1", truncate(btc.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-B", "BTC", "Sell", "1", truncate(btc.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-B", "BTC", "1-April-2022", database);
+						failedTrade(broker, "Strategy-B", "BTC", this.date, database);
 					}
 				}
 				else {
-					badInput(broker, "Strategy-B", "1-April=2022", database);
+					badInput(broker, "Strategy-B", this.date, database);
 				}
 		}
 	} // end of stratB
 
+	/**
+	 * This method is responsible for all Strategy-C broker algorithms.
+	 * 
+	 * @param broker represents the broker name
+	 * @param coinMap represents the map of coins associated with the broker
+	 * @param database represents the action log database where the action will be logged
+	 */
 	private void stratC(String broker, HashMap<String, Coin> coinMap, ActionLog database) {
 
 		switch (broker) {
@@ -211,14 +240,14 @@ public class TradingStrategies {
 
 				if ((btc != null) && (sol != null)) {
 					if ((btc.getPrice() < 59400) && (sol.getPrice() > 130)) {
-						successfulTrade(broker, "Strategy-C", "SOL", "Buy", "5", truncate(sol.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-C", "SOL", "Buy", "5", truncate(sol.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-C", "SOL","1-April-2022",database);
+						failedTrade(broker, "Strategy-C", "SOL",this.date,database);
 					}
 				}
 				else {
-					badInput(broker,"Strategy-C","1-April-2022",database);
+					badInput(broker,"Strategy-C",this.date,database);
 				}
 				break;
 
@@ -230,14 +259,14 @@ public class TradingStrategies {
 
 				if ((eth != null) && (dot != null)) {
 					if ((eth.getPrice() < 4500) && (dot.getPrice() > 20)) {
-						successfulTrade(broker, "Strategy-C", "DOT", "Buy", "4", truncate(dot.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-C", "DOT", "Buy", "4", truncate(dot.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-C", "DOT","1-April-2022",database);
+						failedTrade(broker, "Strategy-C", "DOT",this.date,database);
 					}
 				}
 				else {
-					badInput(broker,"Strategy-C","1-April-2022",database);
+					badInput(broker,"Strategy-C",this.date,database);
 				}
 				break;
 
@@ -249,14 +278,14 @@ public class TradingStrategies {
 
 				if ((usdt != null) && (eos != null)) {
 					if ((usdt.getPrice() < 2) && (eos.getPrice() > 3)) {
-						successfulTrade(broker, "Strategy-C", "EOS", "Buy", "10", truncate(eos.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-C", "EOS", "Buy", "10", truncate(eos.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-C", "EOS","1-April-2022",database);
+						failedTrade(broker, "Strategy-C", "EOS",this.date,database);
 					}
 				}
 				else {
-					badInput(broker,"Strategy-C","1-April-2022",database);
+					badInput(broker,"Strategy-C",this.date,database);
 				}
 				break;
 
@@ -267,18 +296,25 @@ public class TradingStrategies {
 				Coin ada = coinMap.get("cardano");
 				if((doge!= null) && (ada!= null)){
 					if((doge.getPrice()>0.1) && (ada.getPrice() < 150)){
-						successfulTrade(broker, "Strategy-C", "ADA", "Buy", "27", truncate(ada.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-C", "ADA", "Buy", "27", truncate(ada.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-C", "ADA", "1-April-2022", database);
+						failedTrade(broker, "Strategy-C", "ADA", this.date, database);
 					}
 				}
 				else {
-					badInput(broker, "Strategy-C", "1-April-2022", database);
+					badInput(broker, "Strategy-C", this.date, database);
 				}
 		}
 	} // end of strat C
 
+	/**
+	 * This method is responsible for all Strategy-D broker algorithms.
+	 * 
+	 * @param broker represents the broker name
+	 * @param coinMap represents the map of coins associated with the broker
+	 * @param database represents the action log database where the action will be logged
+	 */
 	private void stratD(String broker, HashMap<String, Coin> coinMap, ActionLog database) {
 		switch(broker) {
 			//TODO Broker-1 Bitcoin
@@ -289,14 +325,14 @@ public class TradingStrategies {
 
 				if ((btc != null) && (ada != null)) {
 					if ((btc.getPrice() > 59000) && (ada.getPrice() < 3)) {
-						successfulTrade(broker, "Strategy-D", "ADA", "Sell", "15", truncate(ada.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-D", "ADA", "Sell", "15", truncate(ada.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-D", "ADA","1-April-2022",database);
+						failedTrade(broker, "Strategy-D", "ADA",this.date,database);
 					}
 				}
 				else {
-					badInput(broker,"Strategy-D","1-April-2022",database);
+					badInput(broker,"Strategy-D",this.date,database);
 				}
 				break;
 
@@ -308,14 +344,14 @@ public class TradingStrategies {
 
 				if ((eth != null) && (eos != null)) {
 					if ((eth.getPrice() > 3000) && (eos.getPrice() < 3)) {
-						successfulTrade(broker, "Strategy-D", "EOS", "Sell", "15",truncate(eos.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-D", "EOS", "Sell", "15",truncate(eos.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-D", "EOS","1-April-2022",database);
+						failedTrade(broker, "Strategy-D", "EOS",this.date,database);
 					}
 				}
 				else {
-					badInput(broker,"Strategy-D","1-April-2022",database);
+					badInput(broker,"Strategy-D",this.date,database);
 				}
 				break;
 
@@ -327,14 +363,14 @@ public class TradingStrategies {
 
 				if ((usdt != null) && (dot != null)) {
 					if ((usdt.getPrice() > 3) && (dot.getPrice() < 20)) {
-						successfulTrade(broker, "Strategy-D", "DOT", "Sell", "3", truncate(dot.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-D", "DOT", "Sell", "3", truncate(dot.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-D", "DOT","1-April-2022",database);
+						failedTrade(broker, "Strategy-D", "DOT",this.date,database);
 					}
 				}
 				else {
-					badInput(broker,"Strategy-D","1-April-2022",database);
+					badInput(broker,"Strategy-D",this.date,database);
 				}
 				break;
 
@@ -345,25 +381,46 @@ public class TradingStrategies {
 				Coin ust = coinMap.get("terrausd");
 				if((doge!= null) && (ust!= null)){
 					if((doge.getPrice()>0.1) && (ust.getPrice() < 150)){
-						successfulTrade(broker, "Strategy-D", "UST", "Sell", "10", truncate(ust.getPrice()), "1-April-2022", database);
+						successfulTrade(broker, "Strategy-D", "UST", "Sell", "10", truncate(ust.getPrice()), this.date, database);
 					}
 					else {
-						failedTrade(broker, "Strategy-D", "UST", "1-April-2022", database);
+						failedTrade(broker, "Strategy-D", "UST", this.date, database);
 					}
 				}
 				else {
-					badInput(broker, "Strategy-D", "1-April-2022", database);
+					badInput(broker, "Strategy-D", this.date, database);
 				}
 		}
 	} // end of strat D
 
-	private void successfulTrade(String broker, String Strat, String coin, String action, String amount, String price,String date,ActionLog database) {
-		TradeAction act = new TradeAction(broker, Strat, coin, action, amount, price,date);
+	/**
+	 * This method creates a successful trade action and adds it to the action log database 
+	 * 
+	 * @param broker represents the broker name
+	 * @param strat represents the selected strategy
+	 * @param coin represents the selected coin
+	 * @param action represents the selected action
+	 * @param amount represents the selected amount
+	 * @param price represents the selected price
+	 * @param date represents the trade date
+	 * @param database represents the action log database
+	 */
+	private void successfulTrade(String broker, String strat, String coin, String action, String amount, String price,String date,ActionLog database) {
+		TradeAction act = new TradeAction(broker, strat, coin, action, amount, price,date);
 		int index = database.size();
 		database.addActionLog(Integer.toString(index), act);
 		System.out.println("----------performed successfully----------");
 	}
 
+	/**
+	 * This method creates a failed trade and adds it to the action log database
+	 * 
+	 * @param broker represents the broker name
+	 * @param strat represents the selected strategy
+	 * @param coin represents the selected coin
+	 * @param date represents the trade date
+	 * @param database represents the action log database
+	 */
 	private void failedTrade(String broker, String strat, String coin,String date,ActionLog database) {
 		TradeAction act = new TradeAction(broker, strat, coin, "Fail", "Null", "Null",date);
 		int index = database.size();
@@ -371,6 +428,14 @@ public class TradingStrategies {
 		System.out.println("----------failed successfully----------");
 	}
 
+	/**
+	 * This method creates a failed trade due to bad input and adds it to the action log database
+	 * 
+	 * @param broker represents the broker name
+	 * @param strat represents the selected strategy
+	 * @param date represents the trade date 
+	 * @param database represents the action log database
+	 */
 	private void badInput(String broker, String strat,String date,ActionLog database) {
 		TradeAction act = new TradeAction(broker, strat, "Null", "Fail", "Null", "Null",date);
 		int index = database.size();
@@ -378,9 +443,14 @@ public class TradingStrategies {
 		System.out.println("----------failed successfully (BI)----------");
 	}
 
+	/**
+	 * This method truncates the price to 5 decimal places
+	 * 
+	 * @param price as a double with many digits after the decimal 
+	 * @return price as a string truncated to 5 decimal places
+	 */
 	private String truncate(Double price) {
 		return new DecimalFormat("#.#####").format(price);
 	}
-	
 
 } // end of file
